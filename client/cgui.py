eebf378dd32246ca9e18021a912d82bd8c499365
lmtri2307 <94401753+lmtri2307@users.noreply.github.com>
2021-11-19 03:39:56 +0700

    sever đủ chức năng + handle nhiều client + client hoặc sever tắt không bị treo

--- client/cgui.py
+++ client/cgui.py
@@ -1,12 +1,17 @@
-from socket import AF_INET, socket, SOCK_STREAM
+from io import BufferedReader
+from socket import AF_INET,socket,SOCK_STREAM
+import string
+from threading import Thread
 from tkinter import *
 from tkinter import ttk
 import tkinter
+from urllib.request import DataHandler
 from PIL import Image, ImageTk
 from tkinter import scrolledtext
 import json
 import tkinter.messagebox as mbox
 import re
+
 
 HOST='127.0.0.1'
 PORT=33000
@@ -27,7 +32,6 @@ frame.grid_rowconfigure(0,weight=1)
 frame.grid_columnconfigure(0,weight=1)
 #
 def outputResult(result):
-    print(123445)
     global main_frame
     main_frame=Frame(second_big_frame)
     main_frame.pack(fill=BOTH,expand=1)
@@ -63,7 +67,15 @@ def outputResult(result):
         Label(outFrame, text='   '+result[i]['type']+'   ').grid(column=4,row=n)
         Label(outFrame, text='   '+result[i]['buy']+'   ').grid(column=5,row=n)
         Label(outFrame, text='   '+result[i]['sell']+'   ').grid(column=6,row=n)
-
+def recvResult(conn):
+    length=conn.recv(BUFSIZE).decode(FORMAT)
+    conn.send(bytes(length.encode(FORMAT)))
+    length=int(length)
+    response=""
+    while len(response)<length:
+        response=response+conn.recv(BUFSIZE).decode(FORMAT)
+    list=json.loads(response)
+    return list
 
 def findInfo(INPUT1, INPUT2, value):
     result=[]
@@ -71,17 +83,7 @@ def findInfo(INPUT1, INPUT2, value):
     a=INPUT1.strip("\n")
     b=INPUT2.strip("\n")
     print(len(value))
-    for i in range(0, len(value)):
-        if a!='' and b!='':
-            if value[i]["brand"].lower() == a.lower() and value[i]['company'].lower() == b.lower():
-                result.append(value[i])
-                break
-        if a!='' and b=='':
-            if value[i]["brand"].lower() == a.lower():
-                result.append(value[i])
-        if a=='' and b!='':
-            if value[i]['company'].lower() == b.lower():
-                result.append(value[i])
+    
     print(result)
     print(len(result))
     if len(result)==0:
@@ -95,6 +97,8 @@ def findInfo(INPUT1, INPUT2, value):
 
 def startPage():
     star_page=Frame(frame)
+    my_window.title('Star-page')
+    my_window.geometry('400x300')
     Label(star_page, text="Hi Client !!!",font=("Times New Roman",30) ).pack(pady=30)
     signin=Button(star_page,text="Đăng kí", width=20,command=sigIn).pack(padx=20, pady=10)
     # login=Button(frame,text="Đăng nhập",width=20, command=MainSearch).pack(padx=20, pady=10)
@@ -138,21 +142,41 @@ def clearFrame():
     # my_big_scrollbar.destroy()
 
     # search_page.pack_forget()
-
+def sendOption(option):
+    try:
+        CLIENT.sendall(bytes(option.encode(FORMAT)))
+    except:
+        onErrorLostConnection()
+        startPage()
 
 
 
 def transfer():
-    INPUT1=brand_box.get("1.0",END)
-    INPUT2=company_box.get("1.0",END)
-    print(INPUT1)
-    with open('myfile.json', 'r', encoding='utf-8') as f:
-        data=json.load(f)
-        f.close()
-    x= data['golds']
-    x1=x[0]
-    value=x1['value']
-    findInfo(INPUT1, INPUT2, value)
+    sendOption('SEARCH')
+    INPUT1=brand_box.get()
+    INPUT2=company_box.get()
+    if INPUT1=='':
+        INPUT1='none'
+    if INPUT2=='':
+        INPUT2='none'
+    searchkey=[INPUT1,INPUT2]
+    sendList(searchkey)
+    result=recvResult(CLIENT)
+    if len(result)==0:
+        print('Nothing has this brand')
+        noti_box=Toplevel()
+        noti_box.geometry("400x70")
+        Label(noti_box, text="*Error: Nothing has this Brand=\" " + INPUT1+ '\" Company=\"'+INPUT2+" \"", fg="red", font=("Arial",10, "bold")).pack(expand=True)
+    else:
+        Label(second_big_frame, text="Kết quả trả về cho Brand=\"" + INPUT1+ '\" Company=\"'+INPUT2+" \"", font=("Arial",10,"bold"), fg="#006699").pack()
+        outputResult(result)
+    # with open('myfile.json', 'r', encoding='utf-8') as f:
+    #     data=json.load(f)
+    #     f.close()
+    # x= data['golds']
+    # x1=x[0]
+    # value=x1['value']
+    # findInfo(INPUT1, INPUT2, value)
 
 
 def MainSearch():
@@ -163,16 +187,16 @@ def MainSearch():
     global brand_box
     global company_box
     lbl1=Label(search_page, text="Brand vàng mà bạn muốn tìm: ").pack()
-    brand_box=Text(search_page,width=20,height=1)
+    brand_box=Entry(search_page,width=35)
     brand_box.pack()
     lbl2=Label(search_page, text="Company vàng mà bạn muốn tìm: ").pack()
-    company_box=Text(search_page,width=20,height=1)
+    company_box=Entry(search_page,width=35)
     company_box.pack()
 
     btn_frame=Frame(search_page)
     btn_frame.pack(pady=10, padx=5)
-    submit_button=Button(btn_frame, text="SUBMIT", command=transfer,bg="#333", fg="#fff").grid(column=1, row=1)
-    clear_button=Button(btn_frame, text="Clear all Resuls", command=clearFrame,bg="#333",fg="#fff").grid(column=2, row=1)
+    Button(btn_frame, text="SUBMIT", command=transfer,bg="#333", fg="#fff").grid(column=1, row=1)
+    Button(btn_frame, text="Clear all Resuls", command=clearFrame,bg="#333",fg="#fff").grid(column=2, row=1)
 
     search_page.grid(row=0,column=0,sticky='nsew')
     search_page.tkraise()
@@ -214,11 +238,11 @@ def sigIn():
     input_signin.pack()
     global username_box, password_box
     username=Label(input_signin, text="User Name:").grid(column=1, row=1)
-    username_box=Text(input_signin,width=20,height=1)
+    username_box=Entry(input_signin)
     username_box.grid(column=2, row=1)
     Label(input_signin, text="   ", height=1,font=("Arial",2)).grid(column=1, row=2)
     password=Label(input_signin, text="Password: ").grid(column=1, row=3)
-    password_box=Text(input_signin,width=20,height=1)
+    password_box=Entry(input_signin)
     password_box.grid(column=2, row=3)
     Button(sign_in,text="Sign-in Account",bg="#000", fg="#fff",command=saveNewAcc).pack(pady=20)
     Button(sign_in,text="Back to StarPage",bg="#000", fg="#fff",command=startPage).pack(pady=20)
@@ -228,52 +252,33 @@ def sigIn():
     sign_in.tkraise()
 
 
-def isSameAccount(accounts):
-    for i in range (0,len(accounts)):
-        if(accounts[i]["username"]==user_sign):
-            return True
-    return False
-
 def onErrorSignIn():
-    mbox.showerror("Error", "Tên tài khoản đã được sử dụng, vui lòng thử")
+    mbox.showerror("Error", "Tên tài khoản đã được sử dụng, vui lòng thử lại")
 
 def onErrorLogIn():
     mbox.showerror("Error", "Sai thông tin đăng nhập, vui lòng thử lại")
 
 def successfullSignIn():
-    mbox.showerror("Infomation", "Tạo tài khoản thành công, vui lòng thoát ra và đăng nhập lại")
+    mbox.showinfo("Infomation", "Tạo tài khoản thành công, vui lòng đăng nhập")
 
 def saveNewAcc():
-    global user_sign
-    global password_sign
-    user_sign=username_box.get("1.0", END).strip('\n')
-    print("tên dang kí" + user_sign)
-    password_sign=password_box.get("1.0", END).strip('\n')
-    print("pass dang kí" +password_sign)
-    with open('D:/Destop/testing_python/server/infoclient.json', 'r', encoding='utf8') as f:
-        data=json.load(f)
-        print(data)
-    f.close()
-    accounts=data['account']
-    if(isSameAccount(accounts)==True):
+    user_sign=username_box.get()
+    print("tên dang kí " + user_sign)
+    password_sign=password_box.get()
+    print("pass dang kí " +password_sign)
+    sendOption('SIGNIN')
+    account=[user_sign,password_sign]
+    sendList(account)
+    # with open('D:/Destop/testing_python/server/infoclient.json', 'r', encoding='utf8') as f:
+    #     data=json.load(f)
+    #     print(data)
+    # f.close()
+    response=CLIENT.recv(BUFSIZE).decode(FORMAT)
+    if response=='signin failed':
         onErrorSignIn()
     else:
-        print(123)
-        x={
-            'id':len(accounts)+1,
-            'username': user_sign,
-            'password': password_sign
-        }
-        accounts.append(x)
-        print(accounts)
-        data["account"]=accounts
-        global convertJson
-        convertJson=json.dumps(data)
-        print(convertJson)
-        with open('D:/Destop/testing_python/server/infoclient.json', 'w+') as f:
-            f.write(convertJson)
-            f.close()
         successfullSignIn()
+        logIn()
 
 
 def logIn():
@@ -284,11 +289,11 @@ def logIn():
     input_login=Frame(log_in)
     input_login.pack()
     global username_box_log, password_box_log
-    username=Label(input_login, text="User Name:").grid(column=1, row=1)
+    Label(input_login, text="User Name:").grid(column=1, row=1)
     username_box_log=Entry(input_login)
     username_box_log.grid(column=2, row=1)
     Label(input_login, text="   ", height=1,font=("Arial",2)).grid(column=1, row=2)
-    password=Label(input_login, text="Password: ").grid(column=1, row=3)
+    Label(input_login, text="Password: ").grid(column=1, row=3)
     password_box_log=Entry(input_login)
     password_box_log.grid(column=2, row=3)
     Button(log_in,text="Log-in Account",bg="#000", fg="#fff",command=confirmAcc).pack(pady=20)
@@ -296,20 +301,32 @@ def logIn():
 
     log_in.grid(row=0,column=0,sticky='nsew')
     log_in.tkraise()
+def recvDict(client):
+    # dict={}
+    # key=client.recv(1024).decode(FORMAT)
+    # while (key != 'end dict'):
+    #     client.sendall(key.encode(FORMAT))
+    #     value=''
+    #     value=client.recv(1024).decode(FORMAT)
+    #     dict[key]=value
+    #     client.sendall(key.encode(FORMAT))
+    #     key=client.recv(1024).decode(FORMAT)
+    msg=client.recv(BUFSIZE).encode(FORMAT)
+    
+    print(dict)
+
+    return dict
 
 def sendList( list):
-
     for item in list:
         CLIENT.sendall(item.encode(FORMAT))
         #wait response
         CLIENT.recv(1024)
-
     msg = "end"
     CLIENT.send(msg.encode(FORMAT))
 def confirmAcc():
     # gui option login
-    option='LOGIN'
-    CLIENT.send(option.encode(FORMAT))
+    sendOption('LOGIN')
     global user_log
     global password_log
     user_log=username_box_log.get()
@@ -317,19 +334,38 @@ def confirmAcc():
     # Sever check account
     account=[user_log,password_log]
     sendList(account)
-    check=CLIENT.recv(BUFSIZE).decode(FORMAT)
-    print(check)
-    if check=='accepted':
+    response=CLIENT.recv(BUFSIZE).decode(FORMAT)
+    print(response)
+    if response=='accepted':
         MainSearch()
         return
     
     onErrorLogIn()
 
+def onErrorLostConnection():
+    mbox.showerror("Error", "Sever đã đóng!")
+
 
 # MainSearch()
-__init__()
+def quit():
+    while True:
+        try:
+            pass
+        except OSError:
+            msg='exit'
+            print(msg)
+            CLIENT.send(bytes(msg.encode(FORMAT)))
+#Thread(target=quit,daemon=True).start()
 
+
+def on_closing(event=None):
+    msg='exit'
+    try:
+        CLIENT.send(bytes(msg.encode(FORMAT)))
+        CLIENT.close()
+    finally:
+        my_window.quit()
+my_window.protocol("WM_DELETE_WINDOW", on_closing)
+__init__()
 # saveNewAcc()
 my_window.mainloop()
-
-CLIENT.close()
